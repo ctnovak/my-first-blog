d4b3e2ca66d1c129f4db29e787ac005e8490bb57
Cassandra <cassandra.novak@outlook.com>
2018-09-01 13:46:45 +1000

    My Django Girls app, first commit

---
+++ djangogirlsdraft.py
@@ -0,0 +1,13 @@
+#Creating definitions for a polite code
+
+def hi(name):
+    print('Hi ' + name + '!!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Cass']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1, 4):
+    print(i)
